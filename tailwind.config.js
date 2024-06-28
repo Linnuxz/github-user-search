56a0bd0745e9911387047eea850efaef3d62a15c
Nikoloz Parkosadze <nikaparkosadze4@gmail.com>
2024-06-28 16:44:16 +0400

    add custom utility classes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'custom-gray': '#4B6A9B',
+                'dark-theme': '#1E2A47',
+            },
+            boxShadow: {
+                'light-theme': '0px 16px 30px -10px rgba(70, 96, 187, 0.20)',
+            },
+            caretColor: {
+                blue: '#0079FF',
+            },
+        },
+    },
+    plugins: [],
+};
