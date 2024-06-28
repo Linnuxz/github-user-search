426ea0e6b5793455c5366e4166489fc9bb1cc5fb
Nikoloz Parkosadze <nikaparkosadze4@gmail.com>
2024-06-28 21:11:47 +0400

    alter component structure

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -8,63 +8,71 @@ import CompanyIcon from '/assets/icon-company.svg';
 const ContactInfo = ({ isLightTheme, data }: TComponent) => {
     return (
         <div
-            className={`flex flex-col gap-[16px] ${
+            className={`flex flex-col md:flex-row gap-[16px] ${
                 isLightTheme
                     ? 'text-custom-gray duration-500'
                     : 'text-white duration-500'
             }`}
         >
-            <div
-                className={`flex gap-[19px] items-center ${
-                    !data.location ? 'flex opacity-50' : ''
-                }`}
-            >
-                <img
-                    src={LocationIcon}
-                    className={`w-[13px] h-[20px] ${
-                        !isLightTheme ? 'filter-white duration-500' : ''
+            <div className="flex flex-col gap-[16px]">
+                {/* Location */}
+                <div
+                    className={`flex gap-[19px] items-center ${
+                        !data.location ? 'flex opacity-50' : ''
                     }`}
-                />
-                <p>{data.location || 'Not Available'}</p>
+                >
+                    <img
+                        src={LocationIcon}
+                        className={`w-[13px] h-[20px] ${
+                            !isLightTheme ? 'filter-white duration-500' : ''
+                        }`}
+                    />
+                    <p className='text-[13px] md:text-[15px]'>{data.location || 'Not Available'}</p>
+                </div>
+                {/* URL */}
+                <div
+                    className={`flex gap-[13px] items-center ${
+                        !data.html_url ? 'flex opacity-50' : ''
+                    }`}
+                >
+                    <img
+                        src={WebsiteIcon}
+                        className={`w-[20px] h-[20px] ${
+                            !isLightTheme ? 'filter-white duration-500' : ''
+                        }`}
+                    />
+                    <a href={`${data.html_url}`} target='_blank' className='text-[13px] md:text-[15px] xl:hover:underline'>{data.html_url || 'Not Available'}</a>
+                </div>
             </div>
-            <div
-                className={`flex gap-[13px] items-center ${
-                    !data.html_url ? 'flex opacity-50' : ''
-                }`}
-            >
-                <img
-                    src={WebsiteIcon}
-                    className={`w-[20px] h-[20px] ${
-                        !isLightTheme ? 'filter-white duration-500' : ''
+            {/* Twitter */}
+            <div className="flex flex-col gap-[16px]">
+                <div
+                    className={`flex gap-[13px] items-center ${
+                        !data.twitter_username ? 'flex opacity-50' : ''
                     }`}
-                />
-                <p>{data.html_url || 'Not Available'}</p>
-            </div>
-            <div
-                className={`flex gap-[13px] items-center ${
-                    !data.twitter_username ? 'flex opacity-50' : ''
-                }`}
-            >
-                <img
-                    src={TwitterIcon}
-                    className={`w-[20px] h-[20px] ${
-                        !isLightTheme ? 'filter-white duration-500' : ''
+                >
+                    <img
+                        src={TwitterIcon}
+                        className={`w-[20px] h-[20px] ${
+                            !isLightTheme ? 'filter-white duration-500' : ''
+                        }`}
+                    />
+                    <p className='text-[13px] md:text-[15px]'>{data.twitter_username || 'Not Available'}</p>
+                </div>
+
+                <div
+                    className={`flex gap-[13px] items-center ${
+                        !data.company ? 'flex opacity-50' : ''
                     }`}
-                />
-                <p>{data.twitter_username || 'Not Available'}</p>
-            </div>
-            <div
-                className={`flex gap-[13px] items-center ${
-                    !data.company ? 'flex opacity-50' : ''
-                }`}
-            >
-                <img
-                    src={CompanyIcon}
-                    className={`w-[20px] h-[20px] ${
-                        !isLightTheme ? 'filter-white duration-500' : ''
-                    }`}
-                />
-                <p>{data.company || 'Not Available'}</p>
+                >
+                    <img
+                        src={CompanyIcon}
+                        className={`w-[20px] h-[20px] ${
+                            !isLightTheme ? 'filter-white duration-500' : ''
+                        }`}
+                    />
+                    <p className='text-[13px] md:text-[15px]'>{data.company || 'Not Available'}</p>
+                </div>
             </div>
         </div>
     );
--- src/components/InfoComponent.tsx
+++ src/components/InfoComponent.tsx
@@ -1,22 +1,90 @@
 import Statistics from './Statistics';
 import { TComponent } from '../types';
 import ContactInfo from './ContactInfo';
-import UserInfo from './UserInfo';
 
 const InfoComponent = ({ data, isLightTheme }: TComponent) => {
+    const formatDate = (createdAt: string) => {
+        const date = new Date(createdAt);
+        const day = date.getDate();
+        const month = date.toLocaleString('default', { month: 'short' });
+        const year = date.getFullYear();
+
+        return `Joined ${day} ${month} ${year}`;
+    };
+    const joinData: string = data.created_at as string;
+    const joinedDate = formatDate(joinData);
+
     return (
         <div
             className={`w-full ${
                 isLightTheme
                     ? 'bg-[#FEFEFE] shadow-light-theme duration-500'
                     : 'bg-[#1E2A47] duration-500'
-            } py-[33px] flex gap-[20px] rounded-[15px] px-[24px] flex-col md:px-[40px]`}
+            } py-[33px] flex gap-[20px] rounded-[15px] px-[24px] flex-col md:px-[40px] xl:flex-row`}
         >
-            <UserInfo data={data} isLightTheme={isLightTheme} />
+            {typeof data.avatar_url === 'string' && (
+                <img
+                    src={data.avatar_url}
+                    alt="Avatar"
+                    className="w-[70px] h-[70px] md:w-[117px] md:h-[117px] rounded-full hidden xl:block"
+                />
+            )}
+            <div className="xl:max-w-[480px]">
+                <div className='flex flex-col gap-[18px]'>
+                    <div className="flex items-center gap-[20px] md:gap-[41px]">
+                        {typeof data.avatar_url === 'string' && (
+                            <img
+                                src={data.avatar_url}
+                                alt="Avatar"
+                                className="w-[70px] h-[70px] md:w-[117px] md:h-[117px] rounded-full xl:hidden"
+                            />
+                        )}
+                        <div className="xl:flex xl:justify-between xl:w-full">
+                            <div>
+                                <h2
+                                    className={`${
+                                        isLightTheme
+                                            ? 'text-[#2B3442] duration-500'
+                                            : 'text-white duration-500'
+                                    } text-[16px] md:text-[26px] font-bold`}
+                                >
+                                    {data.name}
+                                </h2>
+                                <p
+                                    className={`text-[#0079FF] text-[13px] md:text-[16px]`}
+                                >
+                                    @{data.login}
+                                </p>
+                            </div>
+                            <p
+                                className={`${
+                                    isLightTheme
+                                        ? 'text-[#697C9A] duration-500'
+                                        : 'text-white duration-500'
+                                } text-[13px] md:text-[15px] xl:mt-2`}
+                            >
+                                {joinedDate}
+                            </p>
+                        </div>
+                    </div>
+                    <p
+                        className={`${
+                            isLightTheme
+                                ? 'text-[#4B6A9B] duration-500'
+                                : 'text-white duration-500'
+                        } text-[13px] md:text-[15px] leading-[25px]`}
+                    >
+                        {data.bio ||
+                            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donecodio. Quisque volutpat mattis eros.'}
+                    </p>
+                </div>
 
-            <Statistics data={data} isLightTheme={isLightTheme} />
+                <div className='flex flex-col gap-[32px] xl:gap-[37px] mt-[23px] xl:mt-[32px]'>
+                    <Statistics data={data} isLightTheme={isLightTheme} />
 
-            <ContactInfo data={data} isLightTheme={isLightTheme} />
+                    <ContactInfo data={data} isLightTheme={isLightTheme} />
+                </div>
+            </div>
         </div>
     );
 };
