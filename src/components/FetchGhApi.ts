6592dd5d773babd4da5b6c19c3bc394c84e8531b
Nikoloz Parkosadze <nikaparkosadze4@gmail.com>
2024-07-20 12:38:13 +0400

    hide api key

--- src/components/FetchGhApi.ts
+++ src/components/FetchGhApi.ts
@@ -1,5 +1,10 @@
-export const fetchApi = async (username:string) => {
-    const response = await fetch(`https://api.github.com/users/${username}`);
-    const data = await response.json()
+export const fetchApi = async (username: string) => {
+    const headers = new Headers({
+        Authorization: `${import.meta.env.NAME_OF_VAR}`,
+    });
+    const response = await fetch(`https://api.github.com/users/${username}`, {
+        headers,
+    });
+    const data = await response.json();
     return data;
 };
